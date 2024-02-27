3fb20243647954d963db30f1da933865211586db
Ting Cao <anita_cao@live.com>
2024-02-26 20:11:38 -0500

    code sync

--- DeepSMOTE/ResNet50_DeepSMOTE_for_mnist.py
+++ DeepSMOTE/ResNet50_DeepSMOTE_for_mnist.py
@@ -1,7 +1,8 @@
 import torchvision
 from torchvision.models import resnet50
 from torchvision.datasets import MNIST
-from tqdm.autonotebook import tqdm
+#from tqdm.autonotebook import tqdm
+from tqdm import tqdm
 from sklearn.metrics import precision_score, recall_score, f1_score, accuracy_score
 import inspect
 import time
@@ -10,15 +11,17 @@ import numpy as np
 from torch import nn, optim
 import torch
 from torchvision import datasets
-from torchvision.transforms import Compose, ToTensor, Normalize, Resize
+from torchvision.transforms import Compose, ToTensor, Normalize, Resize, Grayscale
 from torch.utils.data import DataLoader, Dataset
 from sklearn.model_selection import train_test_split
 import matplotlib.pyplot as plt
+#from torchsummary import summary
 #print(os.getcwd())
 
 #data path 
-#deepsmotepth = '/Users/tcroot/Google Drive/My Drive/Research/DeepSMOTE' #local
-deepsmotepth = '/home/tcvcs/DeepOversample_reference_methods/datasets_files/DeepSMOTE'
+#deepsmotepth = '/Users/tcroot/Google Drive/My Drive/Research/DeepSMOTE' #local in macbook google drive
+#deepsmotepth = '/home/tcvcs/DeepOversample_reference_methods/datasets_files/DeepSMOTE' #server
+deepsmotepth = 'D:/anita/Research/DeepOversample_reference_methods/deepOversample_methods/datasets_files/DeepSMOTE' #local in pc.
 
 imgtype = 'MNIST'
 dtrnimg = deepsmotepth + '/' + imgtype + '/balanced_data/'
@@ -55,7 +58,6 @@ print(collections.Counter(y)) #the dataset should be balanced.
 '''
 #----------------Display mnist data -----------------------
 
-
 random_indices = np.random.choice(X.shape[0], 100, replace=False)
 
 # Use the selected indices to extract the 100 data points
@@ -77,7 +79,7 @@ plt.show()
 
 #define deepsmote balanced minist dataset
 class DeepSMOTE_MNIST(Dataset):
-    def __init__(self, data, labels, transform=None):
+    def __init__(self, data, labels, transform):
         self.data = data
         self.labels = labels
         self.transform = transform
@@ -94,57 +96,49 @@ class DeepSMOTE_MNIST(Dataset):
 
 # Define the transformation for the data
 transform = Compose([
-    ToTensor(), 
-    Normalize((0.5,), (0.5,)),
-    Resize((224, 224)) # Resizing to fit ResNet50 input size
+    Resize((224,224)),  # Resize to 224x224 pixels
+    #Grayscale(num_output_channels=3),  # Convert to 3 color channels
+    ToTensor()  # Convert to tensor
+    #Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])  # Normalize with ImageNet mean and standard deviation
 ])
 
 
 # define model for MNIST
 def getModel():
-  model = resnet50(num_classes = 10)
+  model = resnet50(pretrained = True)
   model.conv1 = nn.Conv2d(1, 64, kernel_size=(7, 7), stride=(2, 2), padding=(3, 3), bias=False)
-  #optimizer = optim.Adadelta(model.parameters())
+  num_ftrs = model.fc.in_features
+  model.fc = nn.Linear(num_ftrs, 10) 
   optimizer = optim.Adam(model.parameters(), lr= 1e-3)
   loss_fn = nn.CrossEntropyLoss()
   return model.to(device), loss_fn, optimizer
 
 def get_data_loader(trail_batch_size, val_batch_size):
-  train_data = DeepSMOTE_MNIST(X_train, y_train, transform)
-  test_data = DeepSMOTE_MNIST(X_test, y_test, transform)
-
+  train_data = DeepSMOTE_MNIST(X_train, y_train, Compose([Resize((224,224)), ToTensor()])
+  test_data = DeepSMOTE_MNIST(X_test, y_test, Compose([Resize((224,224)), ToTensor()])
   train_loader = DataLoader(train_data, batch_size=trail_batch_size, shuffle=True) # Since my total sample size is 4000*0.8, if i choose batch_size 64, i will have about 500 batches. That's why my dataloader will have 500 items(batches).
   test_loader = DataLoader(test_data, batch_size=val_batch_size)
   return train_loader, test_loader
 
-#from sklearn.metrics import classification_report
-def calculate_metric(metric_fn, true_y, pred_y):
+#-----------------Define the metric calculation function---------------------
+from sklearn.metrics import confusion_matrix, accuracy_score
+  
+def calculate_metric(true_y, pred_y):
+    confMat=confusion_matrix(true_y, pred_y)
+    nc=np.sum(confMat, axis=1)
+    tp=np.diagonal(confMat)
+    tpr=tp/nc
+    acsa=np.mean(tpr)
+    gm=np.prod(tpr)**(1/confMat.shape[0])
+    acc=np.sum(tp)/np.sum(nc)
+    return acsa, gm, tpr, confMat, acc
 
-  # multi class problems need to have averaging method
-  # Use inspect to get the signature and arguments of the precision_score function
-  metric_signature = inspect.signature(metric_fn)
-  metric_args = list(metric_signature.parameters.keys())
-
-  #if "average" in inspect.getfullargspec(metric_fn).args:
-  if 'average' in metric_args:
-    # print('here: ', inspect.getfullargspec(metric_fn)), this method getfullargspec is not the correct one, should use inspect.signature instead.
-    return metric_fn(true_y, pred_y, average="weighted")
-  else:
-    return metric_fn(true_y, pred_y)
-
-
-def print_scores(p, r, f1, a, batch_size):
-  # just an utility printing function
-  for name, scores in zip(("precision", "recall", "F1", "accuracy"), (p, r, f1, a)):
-    print(f"\t{name.rjust(14, ' ')}: {sum(scores)/batch_size:.4f}")
-
-def plot_learning_curve(losses):
-  epochs = range(1, len(losses) + 1)
-
+#-----------------Define the plot learning curve function---------------------
+def plot_learning_curve(train_losses,validation_losses):
   # Plotting training and validation loss
   plt.figure(figsize=(12, 5))
-  plt.plot(epochs, losses, label='Training Loss')
-  #plt.plot(epochs, validation_losses, label='Validation Loss')
+  plt.plot(train_losses, label='Training Loss')
+  plt.plot(validation_losses, label='Validation Loss')
   plt.title('Training and Validation Loss')
   plt.xlabel('Epochs')
   plt.ylabel('Loss')
@@ -160,79 +154,67 @@ device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 print("The model will be running on", device, "device")
 
 model, loss_fn, optimizer = getModel()
-# model.eval()
+print(model)
+#model.eval()
 
 train_dl, test_dl = get_data_loader(batch_size, batch_size)
-batches = len(train_dl)
+tra_batches = len(train_dl)
 val_batches = len(test_dl)
 
-losses = []
+
+
+iter=int(epochs*len(train_dl)) #number of iterations, total number of batches
+c=10  #number of classes
+
+acsaSaveTr, gmSaveTr, accSaveTr=np.zeros((iter,)), np.zeros((iter,)), np.zeros((iter,))
+acsaSaveTs, gmSaveTs, accSaveTs=np.zeros((iter,)), np.zeros((iter,)), np.zeros((iter,))
+confMatSaveTr, confMatSaveTs=np.zeros((iter, c, c)), np.zeros((iter, c, c))
+tprSaveTr, tprSaveTs=np.zeros((iter, c)), np.zeros((iter, c))
+
+train_losses, validation_losses = [], []
 
 for epoch in range(epochs):
-  total_loss = 0
-  # progress bar
-  progress = tqdm(enumerate(train_dl), desc='Loss: ', total=batches)
-  #----------------------------TRAINING---------------------------------
-  # set model to training
   model.train()
-
-  for i, data in progress:
-
+  train_loss = 0
+  for i, data in enumerate(train_dl):
     X, y = data[0].to(device), data[1].to(device)
-    #X = X.cuda()
-    #y = y.cuda()
-
-    #trainig step for single batch
-    model.zero_grad() #clear the gradiant
-    outputs = model(X) # pass input data to model and get the output.
-
-    #y = y.to(torch.int64)
-    output = loss_fn(outputs, y.to(torch.int64)) #calculate loss against the true labels.
-
-    output.backward()
+    model.zero_grad()
+    outputs = model(X)
+    loss = loss_fn(outputs, y.to(torch.int64))
+    loss.backward()
     optimizer.step()
-    current_loss = output.item()
-    total_loss += current_loss
-    progress.set_description('Loss: {:.4f}'.format(total_loss/(i+1)))
+    if i % 100 == 0:
+      print(f'Epoch {epoch+1}/{epochs}, batch {i+1}/{tra_batches}, loss: {loss.item()}')
+    train_loss += loss.item()
+  
+  train_losses.append(train_loss/tra_batches)
+  print(f'Epoch {epoch+1}/{epochs}, training loss: {train_loss/tra_batches}')
 
   if torch.cuda.is_available():
     torch.cuda.empty_cache()
-
-  #------------------------------VALIDATION------------------------------
-  val_losses = 0
-  precision, recall, f1, accuracy = [], [], [], []
+  
+  #-----------------Validation---------------------
   model.eval()
-
+  val_loss = 0
   with torch.no_grad():
     for i, data in enumerate(test_dl):
       X, y = data[0].to(device), data[1].to(device)
+      outputs = model(X)
+      val_loss += loss_fn(outputs, y.to(torch.int64)).item()
 
-      # the outputs of the model is a tentor with shape[64, 10], each of the image has 10 different probability indicating it's probability of being 10 different classes.
-      outputs = model(X) # this get's the prediction from the network
+      pLabels = torch.max(outputs, 1)[1] #get class from network's prediction
+      acsa, gm, tpr, confMat, acc = calculate_metric(y.cpu(), pLabels.cpu())
+      acsaSaveTs[epoch*val_batches+i], gmSaveTs[epoch*val_batches+i], accSaveTs[epoch*val_batches+i]=acsa, gm, acc
+      confMatSaveTs[epoch*val_batches+i, :, :]=confMat
+      tprSaveTs[epoch*val_batches+i, :]=tpr
+      print(f'Epoch {epoch+1}/{epochs}, batch {i+1}/{val_batches}, acsa: {acsa}, gm: {gm}, acc: {acc}')
+    validation_losses.append(val_loss/val_batches)
+    print(f'Epoch {epoch+1}/{epochs}, validation loss: {val_loss/val_batches}')
 
-      # out of all the 10 different probablities for each of the images, get the max probability one. That is the predicted class.
-      predicted_classes = torch.max(outputs, 1)[1] # get class from network's prediction
 
-      # Print the NumPy array
-      # numpy_array = predicted_classes.cpu().numpy()
-      # print(numpy_array)
-      # print(collections.Counter(numpy_array))
 
-      val_losses += loss_fn(outputs, y.to(torch.int64))
-
-      # calculate P/R/F1/A metrics for batch
-      for acc, metric in zip((precision, recall, f1, accuracy),(precision_score, recall_score, f1_score, accuracy_score)):
-        acc.append(calculate_metric(metric, y.cpu(), predicted_classes.cpu()))
-
-      #print(classification_report(y.cpu(), predicted_classes.cpu(), digits=4))
-
-  print(f"Epoch {epoch+1}/{epochs}, training loss: {total_loss/batches}, validation loss: {val_losses/val_batches}")
-  print_scores(precision, recall, f1, accuracy, val_batches)
-
-  losses.append(total_loss/batches) # for plotting learning curve
-
-  #losses.append([(val_losses/val_batches), total_loss/batches])
-  #print(validation_losses)
-
-plot_learning_curve(losses)
 print(f"Training time: {time.time()-start_ts}s")
+
+plot_learning_curve(train_losses,validation_losses)
+
+
