9970b02298308b766849b20ff29ce79c14337235
Ting Cao <anita_cao@live.com>
2024-03-02 15:04:26 -0500

    created methods for generating imbalanced celebA, tested

--- DeepSMOTE/Create_Imblanced_datasets.py
+++ DeepSMOTE/Create_Imblanced_datasets.py
@@ -2,12 +2,12 @@
 
 #This file is to create imblanced datasets and store them in txt files for DeepSMOTE (or other methods) to use.
 
-import torch
+#import torch
 from torchvision import datasets, transforms
 from torch.utils.data import DataLoader, Subset,Dataset
 import numpy as np
 import os
-import zipfile
+#import zipfile
 from PIL import Image
 import os
 import pandas as pd
@@ -18,38 +18,27 @@ num_classes = 10
 
 #--------------------------FMNIST-DATA-PROCESSING---------------------------------
 def create_imblanced_fmnist(class_ratios, num_classes):
-  # Define transformations
   transform = transforms.Compose([transforms.ToTensor(),transforms.Normalize((0.5,), (0.5,))])
-  # Load the Fashion MNIST dataset
   train_data = datasets.FashionMNIST(root='./data', train=True, download=True, transform=transform) 
-  
   indices = [] # Create an empty list to store indices for each class
-  
   # Generate indices for imbalanced dataset
   for i in range(num_classes):  # There are 10 classes in Fashion MNIST
     class_indices = np.where(train_data.targets == i)[0]
     imbalance_indices = np.random.choice(class_indices, class_ratios[i], replace=False)
     indices.extend(imbalance_indices) 
-
-  # Create the imbalanced dataset
-  imbalanced_dataset = Subset(train_data, indices)  
+  imbalanced_dataset = Subset(train_data, indices) # Create the imbalanced dataset
   return imbalanced_dataset
 
 # Function to save dataset
-def save_fmnist_txt(image_file, label_file, dataset):
+def save_fmnist_txt(image_file, label_file, imbalanced_dataset):
     with open(image_file, 'w') as img_file, open(label_file, 'w') as lbl_file:
-        for idx in dataset.indices:
-            # Access the original dataset directly
-            image, label = dataset.dataset[idx]
-
-            # Undo normalization and convert to numpy
-            image = image * 0.5 + 0.5
+        for idx in imbalanced_dataset.indices:
+            image, label = imbalanced_dataset.dataset[idx] # Access the original dataset directly
+            image = image * 0.5 + 0.5 # Undo normalization and convert to numpy
             image_array = image.numpy().squeeze()  # Remove channel dim and convert to numpy
-            # Flatten and write the image data
-            image_str = ' '.join(map(str, image_array.flatten()))
-            img_file.write(image_str + '\n')
-            # Write the label
-            lbl_file.write(str(label) + '\n')
+            image_str = ' '.join(map(str, image_array.flatten()))  # Flatten and write the image data
+            img_file.write(image_str + '\n')  # Write the image
+            lbl_file.write(str(label) + '\n')  # Write the label
 
 def load_fmnist_from_txt(image_file_path, label_file_path):
     # Read the image data
@@ -58,39 +47,31 @@ def load_fmnist_from_txt(image_file_path, label_file_path):
     images = np.array(images)
     # Assuming the original shape was 1x28x28 (for Fashion MNIST), reshape accordingly
     images = images.reshape((-1, 1, 28, 28))  # -1 for automatic calculation of batch size
-    
     # Read the label data
     with open(label_file_path, 'r') as lbl_file:
         labels = np.array([int(line.strip()) for line in lbl_file])
-    
     return images, labels
 
 #--------------------------CIFAR10-DATA-PROCESSING---------------------------------
 def create_imblanced_cirfar10(class_ratios, num_classes):
-  # Define transformations
   transform = transforms.Compose([transforms.ToTensor(),
                                   transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
-  # Load the Fashion MNIST dataset
   train_data = datasets.CIFAR10(root='./data', train=True, download=True, transform=transform) 
-  
   indices = []  
   # Generate indices for imbalanced dataset
   for class_id, num_samples in enumerate(class_ratios):
-    # Find indices of all samples belonging to the current class
-    class_indices = np.where(np.array(train_data.targets) == class_id)[0]
-
+    class_indices = np.where(np.array(train_data.targets) == class_id)[0] # Find indices of all samples belonging to the current class
     # Randomly select 'num_samples' indices from the class_indices
     selected_indices = np.random.choice(class_indices, num_samples, replace=False)
     indices.extend(selected_indices)
-  # Create the imbalanced dataset
-  imbalanced_dataset = Subset(train_data, indices)  
+  imbalanced_dataset = Subset(train_data, indices)  # Create the imbalanced dataset
   return imbalanced_dataset
 
 
-def save_cifar10_txt(image_file, label_file, dataset):
+def save_cifar10_txt(image_file, label_file, imbalanced_dataset):
     with open(image_file, 'w') as img_file, open(label_file, 'w') as lbl_file:
-        for idx in dataset.indices:
-            image, label = dataset.dataset[idx]
+        for idx in imbalanced_dataset.indices:
+            image, label = imbalanced_dataset.dataset[idx]
             # Normalize the image to [0, 1] and flatten
             image_array = image.numpy().flatten() * 0.5 + 0.5
             image_str = ' '.join(map(str, image_array))
@@ -115,61 +96,70 @@ def load_cifar10_from_txt(image_file_path, label_file_path):
 # create a celebA dataset based on 5 classes of hair color: blonde, black, bald, brown and gray
 
 # create a dataset class from folder
+
+# Create a custom dataset class from the CelebA dataset
 class CelebAHairColorDataset(Dataset):
     def __init__(self, image_dir, labels, transform=None):
         self.image_dir = image_dir
         self.labels = labels  # A dict mapping image filenames to hair color labels
+        print("labels: ")
+        print(labels)
+        print(len(labels))
         self.transform = transform
+        # Store the image filenames in a list
         self.image_filenames = list(labels.keys())
+        print("list of image_filenames:")
+        print(self.image_filenames)
 
     def __len__(self):
         return len(self.image_filenames)
 
     def __getitem__(self, idx):
         img_name = self.image_filenames[idx]
+        print("img_name:")
+        print(img_name)
         label = self.labels[img_name]
+        print("label:")
+        print(label)
         image = Image.open(os.path.join(self.image_dir, img_name))
-
         if self.transform:
             image = self.transform(image)
-
         return image, label
-
-
 
 
 def create_imblanced_celeba(image_dir,annotations_file):  
     # Define transformations
-    transform = transforms.Compose([transforms.Resize((64, 64)),transforms.ToTensor()])
-    
-    #dataloader = DataLoader(celebA_data, batch_size=32, shuffle=True)
-    celebA_data = datasets.CelebA(root='./data/celeba_imgs/', download=False, transform=transform)
-
+    transform = transforms.Compose([transforms.Resize((224,224)),transforms.ToTensor()])
     hair_color_classes = ['Blond_Hair', 'Black_Hair', 'Bald', 'Brown_Hair', 'Gray_Hair']
     class_ratios = [15000, 1500, 750, 300, 150]
     num_classes = 5
-
-    annotation = pd.read_csv(annotations_file)
+    annotation = pd.read_csv(annotations_file, delim_whitespace=True, skiprows=1)
 
     # Create the imbalanced dataset
     indices = []
+    hair_color_labels = {}
     for class_id, num_samples in enumerate(class_ratios):
         # Find indices of all samples belonging to the current class
         class_indices = np.where(np.array(annotation[hair_color_classes[class_id]]) == 1)[0]
+        print("class_indices: " )
+        print(class_indices)
         # Randomly select 'num_samples' indices from the class_indices
         selected_indices = np.random.choice(class_indices, num_samples, replace=False)
-        indices.extend(selected_indices)
-        # Create the imbalanced dataset
-
-    
-    imbalanced_dataset = Subset(celebA_data, indices)
+        #select the images names from the image_dir based on selected indices and add label to the images and store image name and label pairs into a dictoinary
+        for idx in selected_indices:
+            img_name = annotation.index[idx]
+            hair_color_labels[img_name] = class_id
+    print("hair_color_labels: ")
+    print(hair_color_labels)
+    # Create the imbalanced dataset  
+    imbalanced_dataset = CelebAHairColorDataset(image_dir, hair_color_labels, transform=transform)
     return imbalanced_dataset
 
 # Save the imbalanced dataset to text files
-def save_celeba_txt(image_file, label_file, dataset):
+def save_celeba_txt(image_file, label_file, imbalanced_dataset):
     with open(image_file, 'w') as img_file, open(label_file, 'w') as lbl_file:
-        for idx in dataset.indices:
-            image, label = dataset.dataset[idx]
+        for idx in range(len(imbalanced_dataset)):
+            image, label = imbalanced_dataset.__getitem__(idx)
             # Normalize the image to [0, 1] and flatten
             image_array = image.numpy().flatten()
             image_str = ' '.join(map(str, image_array))
@@ -192,61 +182,75 @@ def load_celeba_from_txt(image_file_path, label_file_path):
 
 
 
-
 #------------TESTING----------------------------------------
 
 # File paths
-deepsmotepth = '/home/tcvcs/datsets_files/DeepSMOTE'
+
+parent_dir = os.path.dirname(os.getcwd())
+deepsmotepth = os.path.join(parent_dir, 'datasets_files', 'DeepSMOTE')
+#deepsmotepth = './datsets_files/DeepSMOTE' #pc local
+#deepsmotepth = '/home/tcvcs/DeepOversample_reference_methods/datsets_files/DeepSMOTE' #server
 imgtype = 'celebA'
+image_0_set_path = os.path.join(deepsmotepth, imgtype, '0')
+image_file_path = os.path.join(image_0_set_path, 'imbalanced_images.txt')
+label_file_path = os.path.join(image_0_set_path, 'imbalanced_labels.txt')
+#image_file_path = deepsmotepth + '/' + imgtype + '/0/' + 'imbalanced_images.txt'
+#label_file_path = deepsmotepth + '/' + imgtype + '/0/' + 'imbalanced_labels.txt'
 
-image_file_path = deepsmotepth + '/' + imgtype + '/0/' + 'imbalanced_images.txt'
-label_file_path = deepsmotepth + '/' + imgtype + '/0/' + 'imbalanced_labels.txt'
-
-image_dir = '/home/tcvcs/data/celeba_imgs'
-annotations_file = '/home/tcvcs/data/list_attr_celeba.txt'
+celeba_data_path = os.path.join(parent_dir, 'data', 'celeba')
+image_dir = os.path.join(celeba_data_path, 'celeba_imgs')
+attributes_path = os.path.join(celeba_data_path, 'list_attr_celeba.txt')
 
 
-# Load the attributes file
-attributes_path = '/home/tcvcs/data/list_attr_celeba.txt'
-attributes_df = pd.read_csv(attributes_path, delim_whitespace=True, skiprows=1)
-print(len(attributes_df))
+
+
 
 hair_color_classes = ['Blond_Hair', 'Black_Hair', 'Bald', 'Brown_Hair', 'Gray_Hair']
 class_ratios = [15000, 1500, 750, 300, 150]
 num_classes = 5
+annotation = pd.read_csv(attributes_path, delim_whitespace=True, skiprows=1)
 
-#create transform for celebA
-transform = transforms.Compose([transforms.Resize((64, 64)),transforms.ToTensor()])
-celebA_data = datasets.CelebA(root='./data/celeba_imgs', download=False, transform=transform)
-
- # Create the imbalanced dataset
+    # Create the imbalanced dataset
 indices = []
+hair_color_labels = {}
 for class_id, num_samples in enumerate(class_ratios):
     # Find indices of all samples belonging to the current class
-    class_indices = np.where(np.array(attributes_df[hair_color_classes[class_id]]) == 1)[0]
+    class_indices = np.where(np.array(annotation[hair_color_classes[class_id]]) == 1)[0]
+    print("class_indices: " )
+    print(class_indices)
     # Randomly select 'num_samples' indices from the class_indices
     selected_indices = np.random.choice(class_indices, num_samples, replace=False)
-    #select the images from the image_dir based on selected_indices and store the images into a txt file
-    indices.extend(selected_indices)
-   
-imbalanced_dataset = Subset(celebA_data, indices)
-print(len(indices))    
-indices = attributes_df[attributes_df[]]
-#print the type shape of attributes_df
+    #select the images names from the image_dir based on selected indices and add label to the images and store image name and label pairs into a dictoinary
+    for idx in selected_indices:
+        img_name = annotation.index[idx]
+        hair_color_labels[img_name] = class_id
+print("hair_color_labels: ")
+print(hair_color_labels)
 
 
 
 
-# Apply filters and store results
-hair_color_images = {color: attributes_df[filter].index.tolist() for color, filter in filters.items()}
 
 
-imbalanced_dataset = create_imblanced_celeba(image_dir,annotations_file)
+
+
+
+imblanced_celeba = create_imblanced_celeba(image_dir,attributes_path)
 print("Imbalanced dataset created successfully.")
 
-# Save the imbalanced dataset to text files
-save_celeba_txt(image_file_path, label_file_path, imbalanced_dataset)
+save_celeba_txt(image_file_path, label_file_path, imblanced_celeba)
 print("Dataset saved to text files successfully.")
 
 # Load the datasets
 images, labels = load_celeba_from_txt(image_file_path, label_file_path)
+print("Dataset loaded successfully.")
+
+# plot the images
+import matplotlib.pyplot as plt
+fig, axes = plt.subplots(1, 5, figsize=(20, 5))
+for i, ax in enumerate(axes):
+    ax.imshow(images[i].transpose(1, 2, 0))
+    ax.set_title(f"Label: {labels[i]}")
+    ax.axis('off')  
+plt.show()
+#------------------------------------------------------------
